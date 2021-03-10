0b17fef5e952ec294bce094a6952317d5ce590c1
quinnciples <49799428+quinnciples@users.noreply.github.com>
2021-03-09 21:58:04 -0700

    Added population and crossover methods.

--- SmartRockets.py
+++ SmartRockets.py
@@ -14,11 +14,14 @@ TWO_PI = 2.0 * math.pi
 # define constants
 WIDTH = 700
 HEIGHT = 500
-FPS = 60
+FPS = 999
 
 ROCKET_WIDTH = 10
 ROCKET_LENGTH = 30
-DNA_LENGTH = 100
+DNA_LENGTH = 600
+MUTATION_RATE = 0.05
+NUM_ROCKETS = 100
+TARGET = (WIDTH // 2, int(HEIGHT * 0.1))
 
 # define colors
 BLACK = (0, 0, 0)
@@ -29,13 +32,20 @@ WHITE = (255, 255, 255)
 def main():
 
     rockets = []
-    # DNA = [Q_Vector2D.random() for _ in range(DNA_LENGTH)]
-    DNA = [Q_Vector2D(4, 0.02) for _ in range(DNA_LENGTH)]
-    print(DNA)
-    rocket = Rocket(x=WIDTH / 2, y=HEIGHT / 2, dna=DNA)
-    # force = Q_Vector2D(angle=0, magnitude=0.1)
-    # rocket.apply_force(force)
-    rockets.append(rocket)
+    obstacles = []
+    targets = []
+    mating_pool = []
+    fitness = []
+
+    obstacle = pygame.Rect(175, 250, 350, 30)
+    obstacles.append(obstacle)
+    target = pygame.Rect(*TARGET, 20, 20)
+    targets.append(target)
+
+    for _ in range(NUM_ROCKETS):
+        DNA = [Q_Vector2D.random() for _ in range(DNA_LENGTH)]
+        rocket = Rocket(x=WIDTH / 2, y=HEIGHT / 8 * 7, dna=DNA)
+        rockets.append(rocket)
 
     # initialize pygame and create screen
     pygame.init()
@@ -54,47 +64,107 @@ def main():
     running = True
 
     while running:
-        # set FPS
-        CLOCK.tick(FPS)
+        NUM_TICKS = 1000
 
-        # clear the screen every time before drawing new objects
-        SCREEN.fill(BLACK)
+        while running and NUM_TICKS:
+            NUM_TICKS -= 1
+            # set FPS
+            CLOCK.tick(FPS)
 
-        # check for the exit
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+            # clear the screen every time before drawing new objects
+            SCREEN.fill(BLACK)
 
+            # check for the exit
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+
+            for rocket in rockets:
+                rocket.apply_force()
+                rocket.update()
+
+                # if rocket.x < 0:
+                #     rocket.x = 0
+                #     rocket.stop()
+                # if rocket.y < 0:
+                #     rocket.y = 0
+                #     rocket.stop()
+                # if rocket.x > WIDTH:
+                #     rocket.x = WIDTH
+                #     rocket.stop()
+                # if rocket.y > HEIGHT:
+                #     rocket.y = HEIGHT
+                #     rocket.stop()
+
+                # rotating the orignal image
+                rotated_rocket = pygame.transform.rotate(ROCKET_TEMPLATE, -rocket.angle_in_degrees)
+                rect = rotated_rocket.get_rect()
+
+                # set the rotated rectangle to the old center
+                rect.center = (rocket.x, rocket.y)
+
+                for obstacle in obstacles:
+                    if rect.colliderect(obstacle) > 0:
+                        rocket.stop()
+
+                for target in targets:
+                    if rect.colliderect(target) > 0:
+                        rocket.stop()
+                        rocket.successful = True
+
+                # drawing the rotated rectangle to the screen
+                SCREEN.blit(rotated_rocket, rect)
+
+            for obstacle in obstacles:
+                pygame.draw.rect(SCREEN, WHITE, obstacle)
+
+            for target in targets:
+                pygame.draw.rect(SCREEN, GREEN, target)
+
+            # flipping the display after drawing everything
+            pygame.display.flip()
+
+        fitness.clear()
         for rocket in rockets:
-            rocket.apply_force()
-            rocket.update()
+            rocket.calculate_fitness(target=TARGET)
+            fitness.append(rocket.fitness)
+        max_fitness = max(fitness)
+        print(f'Max fitness: {max_fitness}')
+        scaler = 1 / max_fitness
 
-            if rocket.x < 0:
-                rocket.x = 0
-                rocket.stop()
-            if rocket.y < 0:
-                rocket.y = 0
-                rocket.stop()
-            if rocket.x > WIDTH:
-                rocket.x = WIDTH
-                rocket.stop()
-            if rocket.y > HEIGHT:
-                rocket.y = HEIGHT
-                rocket.stop()
+        mating_pool.clear()
+        for rocket in rockets:
+            rocket.fitness *= scaler
+            for _ in range(int(rocket.fitness * 1000)):
+                mating_pool.append(rocket.original_dna)
+        random.shuffle(mating_pool)
+        print(f'Mating pool size: {len(mating_pool)}')
 
-            # rotating the orignal image
-            rotated_rocket = pygame.transform.rotate(ROCKET_TEMPLATE, -rocket.angle_in_degrees)
-            rect = rotated_rocket.get_rect()
+        def crossover(mother_dna, father_dna):
+            new_dna = []
+            # dna_sequence = mother_dna + father_dna + mother_dna
+            split_point = random.randint(0, len(mother_dna))
+            # new_dna = dna_sequence[split_point:split_point + len(mother_dna)]
+            for gene in zip(mother_dna, father_dna):
+                if random.random() < 0.5:
+                    # new_dna.append(gene[0])  # Mother DNA
+                    new_dna = mother_dna[0:split_point] + father_dna[split_point:]
+                else:
+                    # new_dna.append(gene[1])  # Mother DNA
+                    new_dna = father_dna[0:split_point] + mother_dna[split_point:]
+            for i in range(len(new_dna)):
+                if random.random() <= MUTATION_RATE:
+                    new_dna[i] = Q_Vector2D.random()
+            assert(len(new_dna) == len(mother_dna) == len(father_dna))
+            return new_dna
 
-            # set the rotated rectangle to the old center
-            rect.center = (rocket.x, rocket.y)
-            # print((rocket.x, rocket.y))
-
-            # drawing the rotated rectangle to the screen
-            SCREEN.blit(rotated_rocket, rect)
-
-        # flipping the display after drawing everything
-        pygame.display.flip()
+        rockets.clear()
+        for _ in range(NUM_ROCKETS):
+            mother_dna = mating_pool.pop()
+            father_dna = mating_pool.pop()
+            DNA = crossover(mother_dna=mother_dna, father_dna=father_dna)
+            rocket = Rocket(x=WIDTH / 2, y=HEIGHT / 8 * 7, dna=DNA)
+            rockets.append(rocket)
 
     pygame.quit()
 
--- rocket.py
+++ rocket.py
@@ -1,9 +1,8 @@
 import math
 import sys
-from typing import MappingView
 sys.path.append("C:\\users\\alexa\\Documents\\Github\\QFunctions")
 from Q_Functions import Q_Vector2D
-from Q_Functions import Q_map
+# from Q_Functions import Q_map
 
 
 class Rocket:
@@ -11,17 +10,24 @@ class Rocket:
         self.velocity = Q_Vector2D(angle=0, magnitude=0)
         self.x = x
         self.y = y
-        self.dna = dna
+        self.dna = [x for x in dna]
+        self.original_dna = [x for x in dna]
+        self.active = True
+        self.fitness = math.inf
+        self.successful = False
 
     def apply_force(self) -> None:
-        if self.dna:
-            acceleration = self.dna.pop(0)
-        else:
-            acceleration = Q_Vector2D(angle=0, magnitude=0)
-        self.velocity = self.velocity + acceleration
+        if self.active:
+            if self.dna:
+                acceleration = self.dna.pop(0)
+            else:
+                acceleration = Q_Vector2D(angle=0, magnitude=0)
+            self.velocity = self.velocity + acceleration
+            self.velocity.limit(2)
 
     def stop(self):
-        self.velocity.magnitude = 0
+        self.velocity.magnitude = 0.0
+        self.active = False
 
     def update(self):
         self.x = self.x + self.velocity.x
@@ -30,6 +36,12 @@ class Rocket:
     @property
     def angle_in_degrees(self):
         return self.velocity.degrees
+
+    def calculate_fitness(self, target):
+        if self.active or self.successful:
+            self.fitness = 1 / (math.sqrt(((target[1] - self.y) ** 2) + ((target[0] - self.x) ** 2)) + 0.0001)
+        else:
+            self.fitness = 0
 
 
 if __name__ == '__main__':
